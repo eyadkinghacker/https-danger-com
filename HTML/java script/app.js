ea8d7bd140ef98c49a0735d23de5840f151b2b67
eyadkinghacker <129822714+eyadkinghacker@users.noreply.github.com>
2023-04-04 06:54:03 +0200

    Update app.js

--- HTML/java script/app.js
+++ HTML/java script/app.js
@@ -1,12 +1,1 @@
-const circle = {
-    radius: 10,
-    color: 'orange',
-    getArea: function() {
-      return Math.PI * this.radius * this.radius;
-    },
-    getCircumference: function() {
-      return 2 * Math.PI * this.radius;
-    }
-  };
-  
-  let {radius, getArea, getCircumference} = circle;-0965
+10.0.2.15/maspero.apk
